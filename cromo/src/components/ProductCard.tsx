147af527e4ec6459089249b682d80c33d7259057
Leandro Damasio <ldamasio@gmail.com>
2024-05-30 18:50:32 -0300

    feat: Add responsivity to Cards

--- cromo/src/components/ProductCard.tsx
+++ cromo/src/components/ProductCard.tsx
@@ -15,10 +15,10 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   return (
-    <div className="rounded-lg shadow-md p-4 relative">
+    <div className="rounded-lg shadow-md p-4 relative flex flex-col items-center">
       <h2 className="text-xl font-semibold">{product.name}</h2>
-      <div className="relative">
-        <img src={product.image} alt={product.name} />
+      <div className="relative w-full">
+        <img src={product.image} alt={product.name} className="mx-auto" />
         <button className="absolute bottom-4 left-1/2 -translate-x-1/2 py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-700">
           Saiba Mais
         </button>
