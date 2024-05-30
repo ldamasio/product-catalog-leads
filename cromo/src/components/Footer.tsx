f27fe225015b679d3f31f0caf8f8c43dbd9eaa0d
Leandro Damasio <ldamasio@gmail.com>
2024-05-30 12:26:53 -0300

    fix class to className

--- cromo/src/components/Footer.tsx
+++ cromo/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer class="p-8">
+    <footer className="p-8">
       <p>&copy; 2024 Cromo Financiamentos. Todos os direitos reservados.</p>
     </footer>
   );
