062aadc276c5f97d4d97fe11e736878656023e18
Leandro Damasio <ldamasio@gmail.com>
2024-06-05 07:38:03 -0300

    feat: Update index

--- cromo/src/components/Volks.tsx
+++ cromo/src/components/Volks.tsx
@@ -1,29 +1,29 @@
-"use client";
+// "use client";
 
-import * as React from 'react';
+// import * as React from 'react';
 
-interface IProps {
-  id: number;
-  name: string;
-  description: string;
-  price: number;
-  image: string;
-  category: string;
-}
+// interface IProps {
+//   id: number;
+//   name: string;
+//   description: string;
+//   price: number;
+//   image: string;
+//   category: string;
+// }
 
-export class Volks extends React.Component<IProps> {
+// export class Volks extends React.Component<IProps> {
 
-  public render(): JSX.Element {
-    return <div>
-      <div className="m-8 p-8">
-        <div className="grid grid-cols-3 gap-4">
-          {productsData.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-        </div>
-      </div>
-    </div>;
-  }
-}
+//   public render(): JSX.Element {
+//     return <div>
+//       <div className="m-8 p-8">
+//         <div className="grid grid-cols-3 gap-4">
+//           {productsData.map((product) => (
+//             <ProductCard key={product.id} product={product} />
+//           ))}
+//         </div>
+//       </div>
+//     </div>;
+//   }
+// }
 
-export default Volks;
+// export default Volks;
