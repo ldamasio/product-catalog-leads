6eb4b61445cc549ff0893bc43e6111e7f27d0efc
Leandro Damasio <ldamasio@gmail.com>
2024-06-12 16:45:49 -0300

    feat: Change Embarcações Financiamento

--- cromo/src/components/Lancha.tsx
+++ cromo/src/components/Lancha.tsx
@@ -7,8 +7,8 @@ interface LanchaProps {
 const Lancha: React.FC<LanchaProps> = () => {
   return (
     <Section
-      title="Financiar uma Lancha"
-      description="Realize o sonho de ter seu barco agora mesmo! Financiamento de lanchas e embarcações com taxas imperdíveis. Realize passeios inesquecíveis com amigos e familiares. Pratique seus esportes aquáticos favoritos."
+      title="Financiar uma Embarcação"
+      description="Realize o sonho de ter seu barco agora mesmo! Financiamento de embarcações com taxas imperdíveis. Realize passeios inesquecíveis com amigos e familiares. Pratique seus esportes aquáticos favoritos."
       imageUrl="lancha.png"
       clipPath=""
     />
