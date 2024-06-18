b744f706f09a79a3fb48e3542054a0036b3fff6c
Leandro Damasio <ldamasio@gmail.com>
2024-06-18 01:27:55 -0300

    Add registration form

--- cromo/src/components/Steps/StepFour.tsx
+++ cromo/src/components/Steps/StepFour.tsx
@@ -4,32 +4,32 @@ const StepThree: React.FC = () => {
 
   return (
 
-    <div  className='container'>
-    <form className='row'>
-      <div className='ten columns terms'>
-        <span>Ao clicar em 'Aceito' eu concordo que:</span>
-        <ul className='docs-terms'>
-          <li>
-            Eu li e concordo com os <a href='#'>Termos de Uso</a>
-          </li>
-          <li>
-            Eu li e aceitei a <a href='#'>Política de Privacidade</a>
-          </li>
-        </ul>
-        <span> 
-          <label>
-            Aceito {': '}
-          <input
-            type='checkbox'
-            autoFocus
-            />
+    <div className='container'>
+      <form className='row'>
+        <div className='ten columns terms'>
+          <span>Ao clicar em 'Aceito' eu concordo que:</span>
+          <ul className='docs-terms'>
+            <li>
+              Eu li e concordo com os <a href='#'>Termos de Uso</a>
+            </li>
+            <li>
+              Eu li e aceitei a <a href='#'>Política de Privacidade</a>
+            </li>
+          </ul>
+          <span>
+            <label>
+              Aceito {': '}
+              <input
+                type='checkbox'
+                autoFocus
+              />
             </label>
-        </span>
-        <button style={{float: 'right'}}>Submit</button>
-      </div>
-    </form>
-  </div>
-);
+          </span>
+          <button style={{ float: 'right' }}>Submit</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default StepThree;
--- cromo/src/components/Steps/StepOne.tsx
+++ cromo/src/components/Steps/StepOne.tsx
@@ -1,31 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
-let state = {
-  firstName: '',
-  lastName: ''
-}
-let firstNameIsInvalid = true
-
-const setFirstName = s => {
-  state.firstName = s
-  firstNameIsInvalid = state.firstName.length === 0
-}
-const setLastName = s => state.lastName = s
-
-export const StepOne = (props) => {
-
-  const validateFirstName = (val) => {
-    let prevFirstName = state.firstName
-    setFirstName(val)
-  }
-
-  const validateLastName = (val) => {
-    setLastName(val)
-  }
-
-  const Required = () =>
-    firstNameIsInvalid ? <span style={{ fontSize: '1rem', color: 'red' }}>&nbsp;[ Required ]</span> : ''
-
+const StepOne: React.FC = () => {
   return (
     <div className='formulario'>
       <h1>Passo 1: Seu Perfil</h1>
@@ -37,8 +12,7 @@ export const StepOne = (props) => {
               className='u-full-width'
               placeholder='Nome Completo'
               type='text'
-              onChange={e => validateFirstName(e.target.value)}
-              value={state.firstName}
+              // value={state.firstName}
               autoFocus
               required
             />
@@ -52,8 +26,7 @@ export const StepOne = (props) => {
               className='u-full-width'
               placeholder='Email'
               type='text'
-              onChange={e => validateFirstName(e.target.value)}
-              value={state.firstName}
+              // value={state.firstName}
               autoFocus
               required
             />
