03d3159fcfb6b7cffee1cfa007368796236db757
Leandro Damasio <ldamasio@gmail.com>
2024-07-14 13:17:40 -0300

    feat: Change User to Detalhes post signal

--- backend/users/signals.py
+++ backend/users/signals.py
@@ -1,11 +1,10 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth import get_user_model
+from .models import Detalhes
 from .utils import send_admin_notification
 
-User = get_user_model()
-
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=Detalhes)
 def send_email_on_user_registration(sender, instance, created, **kwargs):
     if created:
-        send_admin_notification(instance)
+        send_admin_notification(instance.user)
+
